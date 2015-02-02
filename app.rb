a6b6479f0e1b9ff6a2584b45b79d247097433fa0
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-02 12:55:04 -0800

    add code in results.erb file, and update app.rb

--- app.rb
+++ app.rb
@@ -6,3 +6,12 @@ require('./lib/frequency')
 get ('/') do
   erb(:form)
 end
+
+get ('/results') do
+  wordy_string = params.fetch('wordy_string')
+  entered_word = params.fetch('entered_word')
+  @wordy_string = wordy_string
+  @entered_word = entered_word
+  @frequency_count = @wordy_string.frequency(@entered_word)
+  erb(:results)
+end
